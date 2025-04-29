a19228e9a7c2fd16ab1b07e88d7ece60c3923f52
zairradc <zairradc@gmail.com>
2025-04-29 16:51:05 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function updateTime() {
+  const now = new Date();
+  document.getElementById('time').textContent = now.toLocaleTimeString();
+}
+setInterval(updateTime, 1000);
+updateTime();
+
+function openApp(type) {
+  alert(`Open ${type} section... (you’ll replace this with real navigation)`);
+}
